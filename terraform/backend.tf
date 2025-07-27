0f8a832833ea8dd19789874484bb76663d28d69a
isaiah1701 <imichael1403@gmail.com>
2025-07-27 01:55:29 +0100

    tf pipeline fixes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "isaiahbackend01"
-    container_name       = "tfstate"
-    key                  = "infra.tfstate"
+    resource_group_name   = "tfstate-rg"
+    storage_account_name  = "isaiahbackend01"
+    container_name        = "tfstate"
+    key                   = "infra.tfstate"
   }
 }
